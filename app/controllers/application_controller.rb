e60a836f33cd5848ee6322c1ec785062a1716730
Bryce B <bryce.boddy@gmail.com>
2020-07-17 07:58:23 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < Sinatra::Base
+  
+  #CONFIG
+  
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+  end
+  
+  #GET
+  
+  get '/recipes/new' do
+    erb :new
   end
 
   get '/recipes' do
@@ -14,4 +23,35 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+  
+  #PATCH
+  
+  patch '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+  
+  #POST
+  
+  post '/recipes' do
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
+  end
+  
+  #DELETE
+  
+  delete '/recipes/:id' do #destroy action
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect to '/recipes'
+  end
+  
 end
