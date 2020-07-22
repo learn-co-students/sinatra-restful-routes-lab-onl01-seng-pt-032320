e0f623cc10f494372435202454e619e69f01ac0b
arnie b <arnold.bwaila@gmail.com>
2020-07-22 16:27:01 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,34 @@
 class ApplicationController < Sinatra::Base
   configure do
+    set :method_override, 'true'
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes/new' do
+    erb :new
+  end
+  
+  get '/recipes' do 
+    @recipes = Recipe.all
+    if @recipes
+       erb :index
+    else 
+      redirect '/recipes'
+    end
+  end
+  
+  post '/recipes/:id' do 
+    
+  erb :show
+  end
+  
+  patch '/recipes/:id/edit' do 
+  end
+  
+  
+  delete '/recipes/:id' do 
+  end
+  
 
 end
